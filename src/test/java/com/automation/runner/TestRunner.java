6fae11a1c938274303516ccc133d2324f53dca2e
rahul-fed1 <120332711+rahul-fed1@users.noreply.github.com>
2024-07-23 09:59:33 +0530

    Update TestRunner.java

--- src/test/java/com/automation/runner/TestRunner.java
+++ src/test/java/com/automation/runner/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com.automation.steps",
-        plugin = {'html:target/report.html', 'json:target/cucumber.json'"}        
+        plugin = {"html:target/report.html", "json:target/cucumber.json"}        
 )
 public class TestRunner {
 
